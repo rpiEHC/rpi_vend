cd5127cdfaed359343bc5347c8ee7b5f5a56a981
daitomodachi <daitomodachi6@gmail.com>
2012-11-27 11:16:13 -0500

    Update machine.py

--- machine.py
+++ machine.py
@@ -195,8 +195,8 @@ class Purchase(Store):
 
 	# Compute the total dollar amount of the purchase, given the cart
 	def compute_total(self):
-		for x in self.info['cart']:				# Iterate through the cart object
-			self.info['total']+=x.info['cost']*x.info['qty']		# Add the 
+		for x in self.info['cart']:				# Iterate through the cart object which is a list of items
+			self.info['total']+=x.info['cost']*x.info['qty']		# Add the current item cost times the qty
 		return self.info['total']
 
 	# Commit the Purchase to the table
